bf3db1e295baca333b1268bde548e60426bc4a99
cleavecamael <phoenixlee125@gmail.com>
2022-03-05 18:22:06 +0800

    Updated SATSolverTest
    Moved main to SATSolver Test

--- src/main/java/sat/SATSolverTest.java
+++ src/main/java/sat/SATSolverTest.java
@@ -11,55 +11,16 @@ import sat.formula.*;
 
 
 public class SATSolverTest {
-    Literal a = PosLiteral.make("a");
-    Literal b = PosLiteral.make("b");
-    Literal c = PosLiteral.make("c");
-    Literal na = a.getNegation();
-    Literal nb = b.getNegation();
-    Literal nc = c.getNegation();
-
-
-
-	
-	// TODO: add the main method that reads the .cnf file and calls SATSolver.solve to determine the satisfiability
-    
-	
-    public void testSATSolver1(){
-    	// (a v b)
-    	Environment e = SATSolver.solve(makeFm(makeCl(a,b))	);
-/*
-    	assertTrue( "one of the literals should be set to true",
-    			Bool.TRUE == e.get(a.getVariable())  
-    			|| Bool.TRUE == e.get(b.getVariable())	);
-    	
-*/    	
-    }
-    
-    
-    public void testSATSolver2(){
-    	// (~a)
-    	Environment e = SATSolver.solve(makeFm(makeCl(na)));
-/*
-    	assertEquals( Bool.FALSE, e.get(na.getVariable()));
-*/    	
-    }
-    
-    private static Formula makeFm(Clause... e) {
-        Formula f = new Formula();
-        for (Clause c : e) {
-            f = f.addClause(c);
+    public static void main(String[] args) {
+        String filename = args[0];
+        Environment output = SATSolver.solve(CNFReader.readCNF(filename));
+        if (output != null) {
+            writeEnv.writeEnvFile(output, "BoolAssignment.txt");
+            System.out.println("Satisfiable");
+        } else {
+            System.out.println("Unsatisfiable");
         }
-        return f;
+
     }
-    
-    private static Clause makeCl(Literal... e) {
-        Clause c = new Clause();
-        for (Literal l : e) {
-            c = c.add(l);
-        }
-        return c;
-    }
-    
-    
-    
+
 }
